20a9d3f7e3a47562c1a2e633f06aa5ea5cc46436
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2023-03-10 04:31:40 +0800

    Create Progress Bar

--- app/src/main/java/com/example/simplepomodoroapp/MainActivity.java
+++ app/src/main/java/com/example/simplepomodoroapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.simplepomodoroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                finish();
+            }
+        }, 3000);
     }
 }
